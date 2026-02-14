e9a82d26b77557505eb5e0ff21a36d87a83c9de6
Fargus <artiom1996604@gmail.com>
2026-02-14 13:54:05 +0200

    Implemented simple coop

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,13 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    private NetworkManager _networkManager;
+
+    private void Start()
+    {
+    }
+}
