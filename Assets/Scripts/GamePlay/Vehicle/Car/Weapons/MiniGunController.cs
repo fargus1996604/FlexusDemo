71f65b0922e49cde3ea63662b44c51faebfe9ae8
Fargus <artiom1996604@gmail.com>
2026-02-14 22:55:15 +0200

    Added minigun sounds

--- Assets/Scripts/GamePlay/Vehicle/Car/Weapons/MiniGunController.cs
+++ Assets/Scripts/GamePlay/Vehicle/Car/Weapons/MiniGunController.cs
@@ -10,35 +10,50 @@ namespace GamePlay.Vehicle.Car.Weapons
             public Vector3 LookDirection;
             public bool Fire;
         }
-        
+
+        private InputData _data;
+        public InputData Data => _data;
+
         [SerializeField]
         private Transform _leftHandTarget;
+
         public Transform LeftHandTarget => _leftHandTarget;
-        
+
         [SerializeField]
         private Transform _rightHandTarget;
+
         public Transform RightHandTarget => _rightHandTarget;
-        
+
+        [SerializeField]
+        private ParticleSystem _muzzleFlashParticle;
+
         [SerializeField]
         private Transform _gunMesh;
 
         [SerializeField]
         private Transform _barrel;
-        
+
         [SerializeField]
         private int _maxRpm;
 
         [SerializeField]
         private float _power;
-        
+
+        [SerializeField]
+        private float _decreaseSpeed;
+
         [SerializeField]
         private float _currentRpm;
-        
-        private InputData _inputData;
-        
+
+        [SerializeField]
+        private MiniGunSound _sound;
+
+        public bool CanFire => _currentRpm >= _maxRpm;
+        public float Charge => _currentRpm / _maxRpm;
+
         public void SetInputData(InputData inputData)
         {
-            _inputData = inputData;
+            _data = inputData;
         }
 
         public void ResetLookDirection()
@@ -48,10 +63,27 @@ namespace GamePlay.Vehicle.Car.Weapons
 
         public void Tick(float deltaTime)
         {
-            _gunMesh.transform.forward = _inputData.LookDirection;
-            _currentRpm += _inputData.Fire ? _power : -_power;
+            _gunMesh.transform.forward = _data.LookDirection;
+            _currentRpm += (_data.Fire ? _power : -_decreaseSpeed) * deltaTime;
             _currentRpm = Mathf.Clamp(_currentRpm, 0, _maxRpm);
             _barrel.localRotation *= Quaternion.Euler(0, 0, _currentRpm * deltaTime);
+
+            if (CanFire)
+            {
+                if (_muzzleFlashParticle.isPlaying == false)
+                {
+                    _muzzleFlashParticle.Play();
+                }
+            }
+            else
+            {
+                if (_muzzleFlashParticle.isPlaying)
+                {
+                    _muzzleFlashParticle.Play();
+                }
+            }
+
+            _sound.Tick(deltaTime, this);
         }
     }
 }
